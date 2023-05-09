b9aa2d9508b399f3ac3994b07798cb9bbf3e97a3
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-05-09 00:04:43 -0300

    Initial commit

---
+++ app/src/main/java/com/gvfs/vollmed/features/login/LoginFragment.kt
@@ -0,0 +1,40 @@
+package com.gvfs.vollmed.features.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.gvfs.vollmed.R
+import com.gvfs.vollmed.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentLoginBinding.bind(view)
+        
+        binding.formLoginBtnRoute.setOnClickListener {
+            findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/gvfs/vollmed/features/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.gvfs.vollmed.features.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
