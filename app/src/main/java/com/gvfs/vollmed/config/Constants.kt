5d2e529496c4b9bc8b4d5cf0ed161ce73d426311
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-06-08 14:55:03 -0300

    add global Constants

---
+++ app/src/main/java/com/gvfs/vollmed/config/Constants.kt
@@ -0,0 +1,8 @@
+package com.gvfs.vollmed.config
+
+object Constants {
+    const val BASE_URL = "https://voll-med-api-production.up.railway.app/"
+//    const val BASE_URL = "http://localhost:8080/"
+
+    const val LOGIN_ENDPOINT = "login"
+}
