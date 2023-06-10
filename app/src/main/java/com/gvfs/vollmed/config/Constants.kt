693d9478fafeaba436e8d1e3708fbcb95cf5b79c
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-06-10 10:13:16 -0300

    add new constants

--- app/src/main/java/com/gvfs/vollmed/config/Constants.kt
+++ app/src/main/java/com/gvfs/vollmed/config/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
 //    const val BASE_URL = "http://localhost:8080/"
 
     const val LOGIN_ENDPOINT = "login"
+    const val DOCTOR_ENDPOINT = "medicos?size=1000&page=0"
+
+    const val SHARED_PREFERENCES_NAME = "VollMedAuthToken"
+    const val TOKEN_KEY = "token"
 }
